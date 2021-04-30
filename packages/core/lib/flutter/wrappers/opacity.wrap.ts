dbbc4513327d9e3125474c32d83715c949919d1c
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-01 02:45:55 +0900

    update mixin import

--- packages/core/lib/flutter/wrappers/opacity.wrap.ts
+++ packages/core/lib/flutter/wrappers/opacity.wrap.ts
@@ -1,15 +1,18 @@
 import { Opacity, Widget } from "@bridged.xyz/flutter-builder/lib";
-import { ReflectBlendMixin } from "@bridged.xyz/design-sdk/lib/nodes/types";
+import { IReflectBlendMixin } from "@bridged.xyz/design-sdk/lib/nodes/types/mixins";
 
 /**
  * Wrap widget with opacity if possible
  */
-export function wrapWithOpacity(node: ReflectBlendMixin, child: Widget): Widget {
-    if (node.opacity !== undefined && node.opacity !== 1 && child) {
-        return new Opacity({
-            opacity: node.opacity,
-            child: child
-        })
-    }
-    return child;
+export function wrapWithOpacity(
+  node: IReflectBlendMixin,
+  child: Widget
+): Widget {
+  if (node.opacity !== undefined && node.opacity !== 1 && child) {
+    return new Opacity({
+      opacity: node.opacity,
+      child: child,
+    });
+  }
+  return child;
 }
--- packages/core/lib/flutter/wrappers/rotation.wrap.ts
+++ packages/core/lib/flutter/wrappers/rotation.wrap.ts
@@ -1,18 +1,19 @@
 import { Widget, Transform } from "@bridged.xyz/flutter-builder/lib";
-import { ReflectLayoutMixin } from "@bridged.xyz/design-sdk/lib/nodes/types";
+import { IReflectLayoutMixin } from "@bridged.xyz/design-sdk/lib/nodes/types/mixins";
 
 /**
  * https://api.flutter.dev/flutter/widgets/Transform-class.html
  */
-export function wrapWithRotation(node: ReflectLayoutMixin,
-    child: Widget): Widget {
-    if (node.rotation !== undefined && child &&
-        Math.round(node.rotation) !== 0) {
-        //  convert angles to clockwise radians: (angle * -pi/180)
-        Transform.rotate({
-            angle: node.rotation * (-3.14159 / 180),
-            child: child
-        });
-    }
-    return child;
+export function wrapWithRotation(
+  node: IReflectLayoutMixin,
+  child: Widget
+): Widget {
+  if (node.rotation !== undefined && child && Math.round(node.rotation) !== 0) {
+    //  convert angles to clockwise radians: (angle * -pi/180)
+    Transform.rotate({
+      angle: node.rotation * (-3.14159 / 180),
+      child: child,
+    });
+  }
+  return child;
 }
