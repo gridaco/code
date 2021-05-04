99a2163e04deb30880aa6e259bd28a98105fe3ce
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-04 18:12:14 +0900

    fix flutter border radius.only logic

--- packages/designto-flutter/make/border-radius.make.ts
+++ packages/designto-flutter/make/border-radius.make.ts
@@ -4,6 +4,7 @@ import {
   IReflectCornerMixin,
 } from "@bridged.xyz/design-sdk/lib/nodes/types";
 import { interpretRadius } from "../interpreter/radius.interpret";
+import * as core from "@reflect-ui/core";
 
 export function makeBorderRadius(
   node: IReflectCornerMixin
@@ -15,10 +16,26 @@ export function makeBorderRadius(
 
   return node.cornerRadius.all !== undefined
     ? flutter.BorderRadius.circular(node.cornerRadius.all as number)
-    : flutter.BorderRadius.only({
-        topLeft: interpretRadius(node.cornerRadius.tl),
-        topRight: interpretRadius(node.cornerRadius.tr),
-        bottomLeft: interpretRadius(node.cornerRadius.bl),
-        bottomRight: interpretRadius(node.cornerRadius.br),
-      });
+    : _makePartialBorderRadius(node.cornerRadius);
+}
+
+function _makePartialBorderRadius(cornerRadius: core.BorderRadiusManifest) {
+  const _oneofRadiusIsHasValue = [
+    cornerRadius.tl,
+    cornerRadius.tr,
+    cornerRadius.bl,
+    cornerRadius.br,
+  ].some((i) => i !== undefined);
+
+  if (_oneofRadiusIsHasValue) {
+    return flutter.BorderRadius.only({
+      topLeft: interpretRadius(cornerRadius.tl),
+      topRight: interpretRadius(cornerRadius.tr),
+      bottomLeft: interpretRadius(cornerRadius.bl),
+      bottomRight: interpretRadius(cornerRadius.br),
+    });
+  } else {
+    // if none of each corner radius contains value (if every value is empty) do not return a value.
+    return;
+  }
 }
