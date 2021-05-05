0d0bb6bb9798ae1e824b368dfd5d80675c2680fb
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-05 23:50:46 +0900

    init react native builder

---
+++ web-builder/core/widget-with-style.ts
@@ -0,0 +1,4 @@
+/**
+ * Since html based framework's widget can be represented withou any style definition, this WidgetWithStyle class indicates, that the sub instance of this class will contain style data within it.
+ */
+export class WidgetWithStyle {}
---
+++ web-builder/react-native/README.md
@@ -0,0 +1,3 @@
+# React Native builder
+
+> This package is under this "coli-web-builder" repository since it follows the basic web standards and almost overlaps the react-builder's implementation
