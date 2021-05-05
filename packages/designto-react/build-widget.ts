c662380041c3d143b7f56df5c7a0cbb47bd01056
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-05 23:34:01 +0900

    add node tree visualization

--- packages/designto-react/build-widget.ts
+++ packages/designto-react/build-widget.ts
@@ -3,6 +3,10 @@ import { Column, Row, Widget } from "@reflect-ui/core";
 export function buildReactWidgetFromReflectWidget(widget: Widget) {
   if (widget instanceof Column) {
   } else if (widget instanceof Row) {
+    // function row() {
+    //   "flex-direction: row";
+    //   "display: flex";
+    // }
   }
   //
 }
