8784e88cabdc94aefd0f96366ede1d11a895da99
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-05 20:52:19 +0900

    add tokenize

--- packages/designto-token/index.ts
+++ packages/designto-token/index.ts
@@ -1,3 +1,6 @@
+// main
+export { tokenize } from "./main";
+
 // region utils
 export * as utils from "./utils";
 export * as string_utils from "./utils/string";
--- packages/designto-token/main.ts
+++ packages/designto-token/main.ts
@@ -1,12 +1,22 @@
 import { nodes } from "@design-sdk/core";
+import { Container, Widget } from "@reflect-ui/core";
 import { fromRectangle } from "./container";
+
+/**
+ * Main function for converting reflect design node tree to reflect widget token tree
+ */
+export function tokenize(node: nodes.ReflectSceneNode): Widget {
+  return interpretReflectNodesToWidgetTree(node);
+}
 
 function row() {
   "flex-direction: row";
   "display: flex";
 }
 
-function interpretReflectNodesToWidgetTree(node: nodes.ReflectSceneNode) {
+function interpretReflectNodesToWidgetTree(
+  node: nodes.ReflectSceneNode
+): Widget {
   switch (node.type) {
     // case nodes.ReflectSceneNodeType.component:
     //   break;
@@ -15,6 +25,9 @@ function interpretReflectNodesToWidgetTree(node: nodes.ReflectSceneNode) {
     case nodes.ReflectSceneNodeType.rectangle:
       return fromRectangle(node as nodes.ReflectRectangleNode);
       break;
+
+    case nodes.ReflectSceneNodeType.frame:
+      return new Container();
 
     default:
       console.error(`${node.type} is not yet handled by "@designto/token"`);
