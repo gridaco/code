ab98137df7e3ec56387d9d5d86fc1d6614641296
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-31 06:16:40 +0900

    add tree viewer to "to-react"

--- packages/designto-react/build-widget.ts
+++ packages/designto-react/build-widget.ts
@@ -5,24 +5,30 @@ import { IWidgetWithStyle } from "@coli.codes/web-builder-core/widget-with-style
 export function buildReactWidgetFromReflectWidget(
   widget: core.Widget
 ): IWidgetWithStyle {
-  const _reactWidgetChildren = widget.children?.map((c) =>
-    buildReactWidgetFromReflectWidget(c)
-  );
+  const handleChildren = (
+    children: Array<core.Widget>
+  ): Array<IWidgetWithStyle> => {
+    return children?.map((c) => {
+      return buildReactWidgetFromReflectWidget(c);
+    });
+  };
 
   let thisReactWidget: IWidgetWithStyle;
   if (widget instanceof core.Column) {
-    thisReactWidget = new react.Column();
+    thisReactWidget = new react.Column({
+      children: handleChildren(widget.children),
+    });
   } else if (widget instanceof core.Row) {
-    thisReactWidget = new react.Row();
+    thisReactWidget = new react.Row({
+      children: handleChildren(widget.children),
+    });
+  } else if (widget instanceof core.Stack) {
+    thisReactWidget = new react.Stack({
+      children: handleChildren(widget.children),
+    });
   } else {
     // todo - handle case more specific
     thisReactWidget = new react.Container();
-  }
-
-  // finilize and return
-  // set children if possible.
-  if (thisReactWidget instanceof react.ReactMultiChildWidget) {
-    thisReactWidget.children = _reactWidgetChildren;
   }
 
   return thisReactWidget;
