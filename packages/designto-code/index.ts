e885a80188baaf804876a35112dab5b9f2849745
softmarshmallow <woojoo@softmarshmallow.com>
2021-07-10 20:15:08 +0900

    add tokenize and react suppport to main designtocode package

--- packages/designto-code/index.ts
+++ packages/designto-code/index.ts
@@ -1,3 +1,4 @@
 export * as flutter from "./flutter";
 export * as proc from "./proc";
-// export * as react from "./react";
+export * as react from "./react";
+export * as token from "@designto/token";
