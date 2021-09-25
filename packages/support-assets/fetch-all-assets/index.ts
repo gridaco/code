424626d5d1ed722aa54bd2246c9160ba7db8bc7a
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-25 20:50:30 +0900

    protect 429 network error - when loading lots of images

--- packages/support-assets/fetch-all-assets/index.ts
+++ packages/support-assets/fetch-all-assets/index.ts
@@ -6,20 +6,26 @@ import { BaseImageRepositories } from "@design-sdk/core/assets-repository";
 export async function fetch_all_assets(
   asset_repository: BaseImageRepositories<string>
 ) {
-  const fetches: { [key: string]: Promise<string> | string } = {};
-  Object.keys(asset_repository.repositories).map((k) => {
-    const repo = asset_repository.repositories[k];
-    Object.keys(repo.images).forEach((ik) => {
-      const i = repo.images[ik];
-      i.hash && (fetches[i.hash] = asset_repository._fetchDataByHash(i.hash));
-      i.key && (fetches[i.key] = asset_repository.fetchDataById(i.key));
+  try {
+    const fetches: { [key: string]: Promise<string> | string } = {};
+    Object.keys(asset_repository.repositories).map((k) => {
+      const repo = asset_repository.repositories[k];
+      Object.keys(repo.images).forEach((ik) => {
+        const i = repo.images[ik];
+        i.hash && (fetches[i.hash] = asset_repository._fetchDataByHash(i.hash));
+        i.key && (fetches[i.key] = asset_repository.fetchDataById(i.key));
+      });
     });
-  });
 
-  const fetched = {};
-  const keys = Object.keys(fetches);
-  (await Promise.all(Object.values(fetches))).map((v, i, a) => {
-    fetched[keys[i]] = v;
-  });
-  return fetched;
+    const fetched = {};
+    const keys = Object.keys(fetches);
+    (await Promise.all(Object.values(fetches))).map((v, i, a) => {
+      fetched[keys[i]] = v;
+    });
+    return fetched;
+  } catch (_) {
+    // mostly 429 too many request
+    // this can happen when loading image as non-dump but individial promise maps.
+    return {};
+  }
 }
