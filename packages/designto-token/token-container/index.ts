baff7ce9c826073072aa7f38ad9eada271303a77
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-26 08:16:39 +0900

    add fallback visibility

--- packages/designto-token/token-container/index.ts
+++ packages/designto-token/token-container/index.ts
@@ -6,18 +6,18 @@ import * as core from "@reflect-ui/core";
 import { Color } from "@reflect-ui/core";
 import { BoxShape } from "@reflect-ui/core/lib/box-shape";
 import { keyFromNode } from "../key";
-function fromRectangle(node: nodes.ReflectRectangleNode): core.Container {
+function fromRectangle(rect: nodes.ReflectRectangleNode): core.Container {
   const container = new core.Container({
-    key: keyFromNode(node),
+    key: keyFromNode(rect),
     children: undefined,
-    width: node.width,
-    height: node.height,
-    borderRadius: node.cornerRadius,
-    color: forceFillsToSolidColor(node.fills), // FIXME: handle by count of fills.
+    width: rect.width,
+    height: rect.height,
+    borderRadius: rect.cornerRadius,
+    color: forceFillsToSolidColor(rect.fills), // FIXME: handle by count of fills.
   });
 
-  container.x = node.x;
-  container.y = node.y;
+  container.x = rect.x;
+  container.y = rect.y;
   // container.border = new core.Border() as any; // FIXME: handle by count of fills.
   return container;
 }
@@ -26,16 +26,16 @@ function fromEllipse(ellipse: nodes.ReflectEllipseNode): core.Container {
   const container = new core.Container({
     key: keyFromNode(ellipse),
     children: undefined,
+    width: ellipse.width,
+    height: ellipse.height,
+    borderRadius: { all: Math.max(ellipse.width, ellipse.height) / 2 },
+    color: forceFillsToSolidColor(ellipse.fills),
   });
 
   container.x = ellipse.x;
   container.y = ellipse.y;
-  container.width = ellipse.width;
-  container.height = ellipse.height;
-  container.color = forceFillsToSolidColor(ellipse.fills); // FIXME: handle by count of fills.
   container.border = ellipse.strokes as any; // todo
   container.shape = BoxShape.circle;
-  container.borderRadius = { all: Math.max(ellipse.width, ellipse.height) / 2 };
 
   return container;
 }
@@ -46,9 +46,28 @@ export const tokenizeContainer = {
 };
 
 function forceFillsToSolidColor(fills: ReadonlyArray<Figma.Paint>): Color {
-  const fill = retrieveFill(fills, {
+  const fill: Figma.SolidPaint = retrieveFill(fills, {
     onlySolid: true,
   });
-  const color = paintToColor(fill as Figma.SolidPaint);
+  const color = paintToColor(fill);
+
+  // polifill -- some color fills are not supported. in this case since nothing will be visible, we will add a alpha black color to tell user that something is here.
+  if (!color && fills.filter((f) => f.type !== "IMAGE").length > 0) {
+    console.error(
+      `color conversion not supported, falling back to alpha black`
+    );
+    return paintToColor(<Figma.SolidPaint>{
+      type: "SOLID",
+      color: {
+        r: 0,
+        g: 0,
+        b: 0,
+      },
+      opacity: 0.05,
+      visible: true,
+    });
+  }
+  // -------
+
   return color;
 }
