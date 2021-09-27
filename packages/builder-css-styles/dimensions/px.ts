f8eb3c3c4285dd2eb75bee05f23378ed95b12856
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-27 18:22:45 +0900

    add null safety to px

--- packages/builder-css-styles/dimensions/px.ts
+++ packages/builder-css-styles/dimensions/px.ts
@@ -1,5 +1,5 @@
 export function px(d: number): string | undefined {
-  if (d === undefined) {
+  if (d === undefined || d === null) {
     return;
   } else {
     // making to fixed number since css does not support decimal px points.
