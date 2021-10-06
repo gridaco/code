534be1d45f637c7acf92732addf38a1bbb6236c8
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-07 06:59:18 +0900

    parse safety

--- packages/support-flags/index.ts
+++ packages/support-flags/index.ts
@@ -5,11 +5,16 @@ import { ArtworkFlag, artwork_flag_key } from "./--artwork";
 export * from "./types";
 
 export function parse(name: string) {
-  return parseflags(name, [
-    {
-      name: artwork_flag_key,
-      type: "bool",
-    },
-    //. TODO: add other flags under here.
-  ]);
+  try {
+    return parseflags(name, [
+      {
+        name: artwork_flag_key,
+        type: "bool",
+      },
+      //. TODO: add other flags under here.
+    ]);
+  } catch (_) {
+    console.error("error while parsing flags", _);
+    return {};
+  }
 }
