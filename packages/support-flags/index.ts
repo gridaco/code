a4a34f5d0a8057fe4137c02ac919681cd31e5e06
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-08 23:37:44 +0900

    add module flag parsing

--- packages/support-flags/index.ts
+++ packages/support-flags/index.ts
@@ -2,7 +2,7 @@ import { parse as parseflags } from "@design-sdk/flags/parsing-strategy-dashdash
 
 import { flag_key__artwork } from "./--artwork";
 import { flag_key__as_wrap } from "./--as-wrap";
-
+import { flag_key__module } from "./--module";
 export * from "./types";
 
 export function parse(name: string) {
@@ -15,6 +15,10 @@ export function parse(name: string) {
       {
         name: flag_key__as_wrap,
         type: "bool",
+      },
+      {
+        name: flag_key__module,
+        type: "bool", // TODO: support string also.
       },
       //. TODO: add other flags under here.
     ]);
