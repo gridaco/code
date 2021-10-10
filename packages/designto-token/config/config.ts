4e2fd69c35a635732bd98fff9355ce277d42b052
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-10 19:43:43 +0900

    update tokenizer runner config

--- packages/designto-token/config/config.ts
+++ packages/designto-token/config/config.ts
@@ -1,3 +1,6 @@
+import type { Widget } from "@reflect-ui/core";
+import type { ReflectSceneNode } from "@design-sdk/figma-node";
+
 /**
  * A tokenizer config interface for extending tokenizer's behavior.
  *
@@ -26,22 +29,60 @@ export interface TokenizerConfig {
   /**
    * (a.k.a should_stop) a middleware function that will be called before the each tokenization. if returns `true`, the tokenization will stop and return the current value.
    */
-  should_break?: (node: any) => boolean;
+  should_break?: (
+    node: ReflectSceneNode,
+    depth?: number,
+    meta?: CallbackMeta,
+    config?: TokenizerConfig
+  ) => boolean;
 
   /**
    * a middleware function that will be called before the each tokenization. if returns `true`, the tokenization will be skipped.
    */
-  should_skip?: (node: any) => boolean;
+  should_skip?: (
+    node: ReflectSceneNode,
+    depth?: number,
+    meta?: CallbackMeta,
+    config?: TokenizerConfig
+  ) => boolean;
 
   /**
    * ignore a flag feature with a explicit gate.
    * if ignore_flag set to true, the flag will be ignored even if this function returns false.
    * It only affects when `true` returned as a result.
    */
-  should_ignore_flag?: (node: any) => boolean;
+  should_ignore_flag?: (
+    node: ReflectSceneNode,
+    depth?: number,
+    meta?: CallbackMeta,
+    config?: TokenizerConfig
+  ) => boolean;
 
   /**
    * A function that is called in between every tokenization process. (on shot for on node)
    */
-  process_listener?: (node: any) => void;
+  process_listener?: (
+    node: ReflectSceneNode,
+    depth?: number,
+    meta?: CallbackMeta,
+    config?: TokenizerConfig
+  ) => void;
+
+  /**
+   * A custom wrapping logic provider that can generally be use on any scenario.
+   *
+   * you can either return a original, wrapped or a modified token to transform the result, or return false to skip.
+   * returning a value ohter thatn `Widget | fales` will throw - null / undefined / ...
+   */
+  custom_wrapping_provider: (
+    tokenized_child: Widget,
+    node: ReflectSceneNode,
+    depth?: number,
+    meta?: CallbackMeta,
+    config?: TokenizerConfig
+  ) => Widget | false;
 }
+
+type CallbackMeta = {
+  flags?: string[];
+};
