30c6b4e43a18b985ec5482f0a4b508a2b55fca91
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-10 19:43:21 +0900

    update build config interface

--- packages/builder-config/configure/build-config.ts
+++ packages/builder-config/configure/build-config.ts
@@ -1,5 +1,12 @@
-import { TargetCodePlatform } from "./target-build-platform";
-
-export interface Configuration {
-  platforms: TargetCodePlatform[];
+/**
+ * design to code, code gen build configuration
+ */
+export interface BuildConfiguration {
+  /**
+   * Forces to Wraps the final output with a OverflowBox - which makes the widget output's overflow to hidden.
+   * This is useful for baking a preview for a embedded environment. (grida uses this on assistant's non resizable preview - a scale only preview)
+   *
+   * @default false
+   */
+  force_root_widget_fixed_size_no_scroll?: boolean;
 }
