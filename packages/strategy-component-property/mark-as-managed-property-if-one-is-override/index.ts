d2aa83bcbe9548fc48b958b304a4ea610a5eca24
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-15 18:12:35 +0900

    bump packages

--- packages/strategy-component-property/mark-as-managed-property-if-one-is-override/index.ts
+++ packages/strategy-component-property/mark-as-managed-property-if-one-is-override/index.ts
@@ -27,3 +27,16 @@
 export function mark_as_managed_property_if_one_is_override() {
   //
 }
+
+// scoped_design_input_file
+interface ScopedDesignInputFile {
+  readonly file: string;
+  components: {};
+  nodes: {};
+}
+
+// 1.
+// diff first, extract later
+
+// 2.
+// extract with diff
