48241ca586227fc415b6b9a10f16bf87c41672dd
You-j <ijyk3061@gmail.com>
2021-10-19 21:56:57 +0900

    revert px func

--- packages/builder-css-styles/dimensions/px.ts
+++ packages/builder-css-styles/dimensions/px.ts
@@ -2,10 +2,8 @@ export function px(d: number): string | undefined {
   if (d === undefined || d === null) {
     return;
   } else {
-    // https://stackoverflow.com/questions/4308989/are-the-decimal-places-in-a-css-width-respected
     // making to fixed number since css does not support decimal px points.
     // If there is a decimal point, it is rounded to the first place.
-    return `${Math.round(d)}px`;
-    // return `${d.toFixed()}px`;
+    return `${d.toFixed()}px`;
   }
 }
