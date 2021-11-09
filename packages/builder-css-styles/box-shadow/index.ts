0d9bd979ee37fa1814be8af3075555b6a3a2d659
You-j <ijyk3061@gmail.com>
2021-11-10 05:45:32 +0900

    add boxShadow spreadRadius

--- packages/builder-css-styles/box-shadow/index.ts
+++ packages/builder-css-styles/box-shadow/index.ts
@@ -2,14 +2,14 @@ import { BoxShadowManifest } from "@reflect-ui/core/lib/box-shadow";
 import { color } from "../color";
 import { px } from "../dimensions";
 
-export function boxShadow(bs: BoxShadowManifest[]): string {
-  if (bs.length === 0 || !bs) {
+export function boxshadow(bs: BoxShadowManifest[]): string {
+  if (!bs || bs.length === 0) {
     return;
   }
   const res = bs.map((shadow) => {
     return `${px(shadow.offset.dx)} ${px(shadow.offset.dy)} ${px(
       shadow.blurRadius
-    )} ${color(shadow.color)}`;
+    )} ${px(shadow.spreadRadius)} ${color(shadow.color)}`;
   });
 
   return res.toString();
