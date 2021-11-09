30baa1402e6d58991fef9da3292f7fb117e9d911
You-j <ijyk3061@gmail.com>
2021-11-09 22:03:42 +0900

    fix boxShadow attr type to array

--- packages/builder-css-styles/box-shadow/index.ts
+++ packages/builder-css-styles/box-shadow/index.ts
@@ -2,12 +2,15 @@ import { BoxShadowManifest } from "@reflect-ui/core/lib/box-shadow";
 import { color } from "../color";
 import { px } from "../dimensions";
 
-export function boxshadow(bs: BoxShadowManifest): string {
-  if (!bs) {
+export function boxShadow(bs: BoxShadowManifest[]): string {
+  if (bs.length === 0 || !bs) {
     return;
   }
+  const res = bs.map((shadow) => {
+    return `${px(shadow.offset.dx)} ${px(shadow.offset.dy)} ${px(
+      shadow.blurRadius
+    )} ${color(shadow.color)}`;
+  });
 
-  return `${px(bs.offset.dx)} ${px(bs.offset.dy)} ${px(bs.blurRadius)} ${color(
-    bs.color
-  )}`;
+  return res.toString();
 }
