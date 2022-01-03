8c3af519c9b5b53387e416e9e7e8766294254748
softmarshmallow <woojoo@softmarshmallow.com>
2022-01-04 02:46:29 +0900

    (gh-actions trigger) update rel path resolution

--- scripts/docs-deploy/pre-deploy.sh
+++ scripts/docs-deploy/pre-deploy.sh
@@ -1,4 +1,4 @@
-# scripts dir
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-node $parent_path/docs-copy/flags-api-docs.js
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR/.." # scripts/
+node ./docs-copy/flags-api-docs.js
