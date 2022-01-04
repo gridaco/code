d81060cb148f821daa0f536bf823415aac5dec49
softmarshmallow <woojoo@softmarshmallow.com>
2022-01-04 10:05:32 +0900

    (ci) asyncfy the copy command

--- scripts/docs-copy/flags-api-docs.js
+++ scripts/docs-copy/flags-api-docs.js
@@ -4,11 +4,10 @@ const fse = require("fs-extra");
 const origin = path.join(__dirname, "../../packages/support-flags/docs");
 const target = path.join(__dirname, "../../docs/flags");
 
+async function main() {
+  await fse.copy(origin, target, { overwrite: true });
+}
+
 if (require.main === module) {
-  fse.copySync(origin, target, { overwrite: true }, function (err) {
-    if (err) {
-      console.error(err);
-    } else {
-    }
-  });
+  main();
 }
