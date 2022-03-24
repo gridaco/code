de32f50daa544e28aa9913fe2bbd98f1c6f911e2
softmarshmallow <woojoo@softmarshmallow.com>
2022-03-24 23:18:01 +0900

    add autofocus flag signature

--- packages/support-flags/--artwork/index.ts
+++ packages/support-flags/--artwork/index.ts
@@ -1,5 +1,13 @@
 export const flag_key__artwork = "artwork";
 
+const flag_key__as_artwork = "as-artwork";
+
+export const flag_key_alias__artwork = [
+  //
+  flag_key__artwork,
+  flag_key__as_artwork,
+] as const;
+
 export interface ArtworkFlag {
   flag: typeof flag_key__artwork;
   value?: boolean;
