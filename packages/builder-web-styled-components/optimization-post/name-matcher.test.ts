05a6ba55cda1ffbaf538a330c0f00bbb9d9d8ff4
UZU, J <woojoo@softmarshmallow.com>
2022-04-05 20:09:07 +0900

    add tests

--- packages/builder-web-styled-components/optimization-post/name-matcher.test.ts
+++ packages/builder-web-styled-components/optimization-post/name-matcher.test.ts
@@ -12,4 +12,12 @@ test("is_matching_name", () => {
   expect(
     is_matching_name("Rectangle826", "Rectangle827", ["suffix-number"])[0]
   ).toBe(true);
+
+  expect(
+    is_matching_name("Card1_0001", "Card2_0001", [
+      "exact",
+      "suffix-number",
+      "suffix-separator-number",
+    ])
+  ).toBe(false);
 });
