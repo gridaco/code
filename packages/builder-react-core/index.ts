fbeba02eb5d678b7bce829a77330254500efbada
softmarshmallow <woojoo@softmarshmallow.com>
2022-05-11 11:23:39 +0900

    rm invalid import

--- packages/builder-react-core/index.ts
+++ packages/builder-react-core/index.ts
@@ -1,6 +1,5 @@
 export * from "./export-result";
 export * from "./react-component-exporting";
 export * from "./react-import-specifications";
-export * from "../builder-web-react/react-project";
 export * from "./react-module-file";
 export * from "./react-module";
