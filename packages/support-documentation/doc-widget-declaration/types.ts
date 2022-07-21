0ce5dabb540a3b542e18bd899b8a65ba09d941b9
softmarshmallow <woojoo@softmarshmallow.com>
2022-07-21 16:26:25 +0900

    add widget declaration info type for doc

--- packages/support-documentation/doc-widget-declaration/types.ts
+++ packages/support-documentation/doc-widget-declaration/types.ts
@@ -1,5 +1,12 @@
-import type { Literal, Parameter } from "coli";
+import type { EsComponentExportingCofnig } from "@designto/config/module-es";
+import type { FunctionDeclaration, Literal, Parameter } from "coli";
 
 export interface WidgetModuleInfo {}
 export type PropsInfo = Array<Parameter>;
 export type PropsDefaultValues = { [key: string]: Literal };
+export type WidgetDeclarationInfo =
+  | FunctionDeclaration
+  | {
+      type: EsComponentExportingCofnig["type"] | "unknown";
+      identifier: string;
+    };
