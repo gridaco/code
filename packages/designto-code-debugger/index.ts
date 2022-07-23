696c87a5e73aa0621263917727ce0df4061ea906
softmarshmallow <woojoo@softmarshmallow.com>
2022-07-23 15:33:34 +0900

    add mute, unmute command with postmessage interface to debugger

--- packages/designto-code-debugger/index.ts
+++ packages/designto-code-debugger/index.ts
@@ -1,5 +1,28 @@
+const IS_DEV = process.env.NODE_ENV === "development";
+let MUTED = false;
+
+// if browser
+if (typeof window !== "undefined") {
+  ///
+  /// on console, type
+  /// - window.postMessage("mute") to mute
+  /// - window.postMessage("unmute") to unmute
+  ///
+  addEventListener("message", function (event) {
+    var data = event.data;
+    if (data === "mute") {
+      MUTED = true;
+      console.log("muted");
+    }
+    if (data === "unmute") {
+      MUTED = false;
+      console.log("unmuted");
+    }
+  });
+}
+
 export function debug(...messages) {
-  if (process.env.NODE_ENV === "development") {
+  if (!MUTED && IS_DEV) {
     console.log(...messages);
   }
 }
