22cb0e18675788f142d967a4a99e49557afa1d54
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-01 02:32:54 +0900

    update framework config exporting & more

--- packages/builder-config/index.ts
+++ packages/builder-config/index.ts
@@ -3,4 +3,5 @@ export * as output from "./output";
 export * as config from "./configure"; // configure
 export * from "./configure"; // configure
 export * from "./framework";
+export * from "./designsource";
 export * as k from "./k";
