c943d0056cd78247722b27fe0dce83d6f62d1d32
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-01 12:25:03 +0900

    update test

--- cli/project/__test__/find-project.test.ts
+++ cli/project/__test__/find-project.test.ts
@@ -1,19 +1,26 @@
-import { find_grida_project } from "../index";
+import { locateProject, read } from "../index";
 import path from "path";
 
 test("find grida project cwd", () => {
-  const _ = find_grida_project(__dirname);
+  const _ = locateProject(__dirname);
   expect(_?.config_file).toBe(path.join(__dirname, "grida.config.js"));
 });
 
 test("find grida project from subdir", () => {
-  const _ = find_grida_project(path.join(__dirname, "sub-dir-for-testing"));
+  const _ = locateProject(path.join(__dirname, "sub-dir-for-testing"));
   expect(_?.config_file).toBe(path.join(__dirname, "grida.config.js"));
 });
 
 test("find grida project from subdir (deep)", () => {
-  const _ = find_grida_project(
+  const _ = locateProject(
     path.join(__dirname, "sub-dir-for-testing", "another-nested-dir")
   );
   expect(_?.config_file).toBe(path.join(__dirname, "grida.config.js"));
+});
+
+test("read config", () => {
+  const _ = locateProject(
+    path.join(__dirname, "sub-dir-for-testing", "another-nested-dir")
+  );
+  expect(read(_?.config_file).name).toBe("__test__");
 });
--- cli/project/__test__/grida.config.js
+++ cli/project/__test__/grida.config.js
@@ -1,2 +1,4 @@
 // THIS IS A DUMMY grida.config.js FILE FOR TESTING
-module.exports = {};
+module.exports = {
+  name: "__test__",
+};
