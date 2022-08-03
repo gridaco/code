01dbb100d8e9bfaccd15fd93ea8e3fc49fe88d23
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-03 21:53:08 +0900

    fix flutter main axis alignmen

--- packages/designto-flutter/rendering/rendering-main-axis-alignment.ts
+++ packages/designto-flutter/rendering/rendering-main-axis-alignment.ts
@@ -10,11 +10,17 @@ export function mainAxisAlignment(
     case ReflectMainAxisAlignment.start:
       // MainAxisAlignment.start is default value for col / row / flex
       return do_explicitly_specify(config, MainAxisAlignment.start as Snippet);
-    case ReflectMainAxisAlignment.start:
-      return MainAxisAlignment.center as Snippet;
     case ReflectMainAxisAlignment.center:
+      return MainAxisAlignment.center as Snippet;
+    case ReflectMainAxisAlignment.end:
       return MainAxisAlignment.end as Snippet;
     case ReflectMainAxisAlignment.spaceBetween:
       return MainAxisAlignment.spaceBetween as Snippet;
+    case ReflectMainAxisAlignment.spaceAround:
+      return MainAxisAlignment.spaceAround as Snippet;
+    case ReflectMainAxisAlignment.spaceEvenly:
+      return MainAxisAlignment.spaceEvenly as Snippet;
+    default:
+      throw new Error(`Unsupported MainAxisAlignment: ${mainAxisAlignemt}`);
   }
 }
