b534be5b8b1dfd96071b71ef464ec5f659c57aa7
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-05 12:44:07 +0900

    fix nested stack position handling

--- packages/designto-flutter/case-handling/handle-nested-stack/index.ts
+++ packages/designto-flutter/case-handling/handle-nested-stack/index.ts
@@ -1,12 +1,24 @@
+import type { SnapshotWidget } from "@designto/token";
 import * as flutter from "@flutter-builder/flutter";
+import * as core from "@reflect-ui/core";
 
 export function handle_flutter_case_nested_positioned_stack(
-  stack: flutter.Stack
+  stack: flutter.Stack,
+  context: {
+    widget: SnapshotWidget<core.Stack>;
+    explicit_width?: boolean;
+    explicit_height?: boolean;
+  }
 ): flutter.Container {
+  const { explicit_width, explicit_height } = context;
+  const { snapshot: original } = context.widget;
+
   return new flutter.Container({
-    // TODO: do we need this?
-    width: flutter.MediaQuery.of().size.width,
-    height: flutter.MediaQuery.of().size.height,
+    key: new flutter.Key(
+      "container for nested stack of " + context.widget.key.id
+    ),
+    width: explicit_width ? original.width : undefined,
+    height: explicit_height ? original.height : undefined,
     child: stack,
   });
 }
