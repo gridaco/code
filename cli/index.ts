dd1c71262cefeaa75ab45467d0c09f482911f118
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-13 19:27:15 +0900

    add global error message handling

--- cli/index.ts
+++ cli/index.ts
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
 
 import cli from "./bin";
+import chalk from "chalk";
 export type { GridaConfig } from "./config";
 
-process.on("SIGINT", () => {
-  process.exit(0); // now the "exit" event will fire
-});
+process
+  .on("SIGINT", () => {
+    process.exit(0); // now the "exit" event will fire
+  })
+  .on("uncaughtException", (err) => {
+    console.log(chalk.bgRed(err.message ?? ""));
+    process.exit(1);
+  })
+  .on("unhandledRejection", (err: Error, p) => {
+    console.error(chalk.bgRed(err.message ?? ""));
+    process.exit(1);
+  });
 
 // if main
 if (require.main === module) {
