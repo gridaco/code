68169b7f5b2fb8578ebf27529424d02440e225fd
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-30 04:47:46 +0900

    fix .env syncer

--- cli/scripts/copy-env.js
+++ cli/scripts/copy-env.js
@@ -5,18 +5,23 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const dist = path.resolve(__dirname, "../dist");
-const publicCredentials = path.resolve(__dirname, "../.public-credentials");
-const distPublicCredentials = path.resolve(dist, ".public-credentials");
 
-// make .public-credentials dir under dist
-fs.mkdirSync(path.resolve(dist, ".public-credentials"));
+function sync_target(target) {
+  const to = path.resolve(__dirname, `../${target}`);
+  const tt = path.resolve(dist, target);
+  // make (e.g. .public-credentials) dir under dist
+  fs.mkdirSync(path.resolve(dist, target));
+  // copy entire (e.g. public-credentials) folder to dist
+  fs.copySync(to, tt);
+  // ensure .env exists in dist
+  fs.ensureFileSync(path.resolve(tt, ".env"));
+}
+const _public_credentials = ".public-credentials";
+const _runtime_env = ".runtime-env";
 
-// copy entire public-credentials folder to dist
-fs.copySync(publicCredentials, distPublicCredentials);
-
-// ensure .env exists in dist
 try {
-  fs.ensureFileSync(path.resolve(distPublicCredentials, ".env"));
+  sync_target(_public_credentials);
+  sync_target(_runtime_env);
 } catch (e) {
   console.error(
     "Oops. you cannot run copy-env unless you are maintainer of this project."
