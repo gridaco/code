c56a3c098e2d8b634a9b834080d13145bfefe2da
softmarshmallow <woojoo@softmarshmallow.com>
2022-11-03 02:56:23 +0900

    color chip & gradient chip

--- packages/builder-css-styles/index.ts
+++ packages/builder-css-styles/index.ts
@@ -10,18 +10,19 @@ export * from "./font-family";
 export * from "./text-decoration";
 export * from "./text-shadow";
 export * from "./text-transform";
-export * from "./gradient";
 export * from "./padding";
 export * from "./margin";
 export * from "./position";
 export * from "./justify-content";
 export * from "./min-height";
 export * from "./length";
+export * from "./linear-gradient";
 export * from "./letter-spacing";
 export * from "./calc";
 export * from "./opacity";
 export * from "./blur";
 export * from "./rotation";
+export * from "./radial-gradient";
 
 // shapes & tricks  ---------------------------------------------------------------
 export * as tricks from "./tricks";
