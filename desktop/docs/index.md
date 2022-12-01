c5da31c8cb58a58f8a1e479551d9be9cc7d9aef6
Universe <woojoo@softmarshmallow.com>
2022-12-01 13:10:51 +0900

    update brand names

---
+++ desktop/docs/index.md
@@ -0,0 +1,8 @@
+# Desktop features documentation
+
+- Open local files
+- Local FS Access
+- childprocess spwan
+- multi tabs
+- auxillary windows
+- perm auth (keytar)
--- desktop/src/main/menu.ts
+++ desktop/src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Grida Code',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Grida Code',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
