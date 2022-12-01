99c84e72ecc2e65c0f35d8b07b8e3d0fbb0203da
Universe <woojoo@softmarshmallow.com>
2022-12-01 13:18:29 +0900

    update icons

--- desktop/src/renderer/App.tsx
+++ desktop/src/renderer/App.tsx
@@ -1,13 +1,9 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
 
 const Hello = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
         <a
