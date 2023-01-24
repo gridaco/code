0dc4cc36557fbb1d32df2cc49b4197481d997114
softmarshmallow <woojoo@softmarshmallow.com>
2023-01-24 20:25:21 +0900

    configure new package

--- vscode/embeddings/next.config.js
+++ vscode/embeddings/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: [
+    "@code-editor/preview-pip",
+    "@code-editor/monaco",
+    "@code-editor/utils",
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
