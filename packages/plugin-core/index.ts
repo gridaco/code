285b6f096fde940fe0c6f5593c3138c72f5645f0
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-12 16:33:36 +0900

    update plugin core

--- packages/plugin-core/index.ts
+++ packages/plugin-core/index.ts
@@ -1,9 +1,28 @@
+import { Framework } from "@grida/builder-platform-types";
+
+type PluginHook = {
+  tap: (name: string, fn: (config: any) => void) => void;
+};
+
+type CodeLifeCycleContext = {
+  hooks: {
+    afterVanillaCSSBundle: PluginHook;
+  };
+};
+
 interface ICodePluginBase {
-  apply(context: any): void;
+  framework: Framework | "*";
+  apply(context: CodeLifeCycleContext): void;
 }
 
 export default abstract class Plugin implements ICodePluginBase {
-  apply(context: any) {}
+  framework: Framework | "*";
+
+  constructor(args: { framework: Framework | "*" }) {
+    this.framework = args.framework;
+  }
+
+  abstract apply(context: any): void;
 }
 
 export type UseCodePluginByPresetNameSpec = string;
