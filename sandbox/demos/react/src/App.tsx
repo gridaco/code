674aeeaee203aedd46cd3ec78644cc153fe93ebc
softmarshmallow <woojoo@softmarshmallow.com>
2023-05-23 17:42:45 +0900

    add simple react app with index.html for vite test

---
+++ sandbox/demos/react/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
