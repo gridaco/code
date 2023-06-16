e998fd92eb47f67c49958d4af86e7d8d2556d686
Universe <woojoo@softmarshmallow.com>
2023-06-17 04:32:54 +0900

    reports server editor api client

--- testing/testing-editor-client/index.ts
+++ testing/testing-editor-client/index.ts
@@ -1,14 +1,2 @@
-import Axios from "axios";
-
-const axios = Axios.create({
-  baseURL: "http://localhost:3000",
-});
-
-function reports() {
-  //
-}
-
-function screenshot_htmlcss() {
-  axios.post("/screenshots/htmlcss", {});
-  //
-}
+export * from "./client";
+export * from "./types";
