fca57071a59bf7f457da2d7132105d9fbee3e915
softmarshmallow <woojoo@softmarshmallow.com>
2023-06-18 07:12:23 +0900

    mv

---
+++ packages/designto-token/token-background/background-image.ts
@@ -0,0 +1,17 @@
+import { Figma } from "@design-sdk/figma-types";
+
+export function tokenize_background_image(paint: Figma.ImagePaint) {
+  // TODO:
+  return {};
+}
+
+/**
+ * handle background source with stacking
+ */
+export function withStacking() {
+  //
+}
+
+function background_image_as_image_widget() {}
+
+function background_video_as_video_widget() {}
