b023e5a8752fedea341f2b16ecb3d71a2438ff56
softmarshmallow <woojoo@softmarshmallow.com>
2023-06-20 04:34:00 +0900

    optional env config

--- testing/report/report.config.js
+++ testing/report/report.config.js
@@ -9,9 +9,10 @@ const { OUTDIR, LOCAL_ARCHIVE_FILES, LOCAL_ARCHIVE_IMAGES } = process.env;
 module.exports = {
   sample: path.join(__dirname, "../../data/figma-archives/prod/meta.json"),
   outDir: OUTDIR,
-  localarchive: {
-    files: LOCAL_ARCHIVE_FILES,
-    images: LOCAL_ARCHIVE_IMAGES,
-  },
-  skipIfReportExists: true,
+  localarchive: !!LOCAL_ARCHIVE_FILES &&
+    !!LOCAL_ARCHIVE_IMAGES && {
+      files: LOCAL_ARCHIVE_FILES,
+      images: LOCAL_ARCHIVE_IMAGES,
+    },
+  skipIfReportExists: false,
 };
